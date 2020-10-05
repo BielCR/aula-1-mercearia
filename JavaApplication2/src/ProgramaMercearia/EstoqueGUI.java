c85351ababa49656d9ab39829fceefb3d25fb9bf
gabri <gabri@LAPTOP-LUHHDP87>
2020-10-05 08:33:49 -0300

    Painel de estoque (parte 1)

--- JavaApplication2/src/ProgramaMercearia/EstoqueGUI.java
+++ JavaApplication2/src/ProgramaMercearia/EstoqueGUI.java
@@ -27,19 +27,141 @@ public class EstoqueGUI extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        nomePesquisaTF = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        nomePesquisaBT = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        estoqueTabela = new javax.swing.JTable();
+        voltarBT = new javax.swing.JButton();
+        excluiBT = new javax.swing.JButton();
+        novoBT = new javax.swing.JButton();
+
+        setBackground(new java.awt.Color(255, 255, 255));
+        setPreferredSize(new java.awt.Dimension(733, 405));
+
+        nomePesquisaTF.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                nomePesquisaTFActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel1.setText("Nome:");
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel3.setText("Estoque");
+
+        nomePesquisaBT.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        nomePesquisaBT.setText("Buscar");
+        nomePesquisaBT.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                nomePesquisaBTActionPerformed(evt);
+            }
+        });
+
+        estoqueTabela.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(estoqueTabela);
+
+        voltarBT.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        voltarBT.setText("Voltar");
+        voltarBT.setAlignmentY(0.0F);
+        voltarBT.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                voltarBTActionPerformed(evt);
+            }
+        });
+
+        excluiBT.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        excluiBT.setText("Excluir");
+        excluiBT.setAlignmentY(0.0F);
+
+        novoBT.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        novoBT.setText("Novo");
+        novoBT.setAlignmentY(0.0F);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 828, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(66, 66, 66)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(nomePesquisaTF)
+                        .addGap(18, 18, 18)
+                        .addComponent(nomePesquisaBT))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 562, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                    .addComponent(excluiBT)
+                    .addComponent(novoBT)
+                    .addComponent(voltarBT))
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(318, 318, 318)
+                .addComponent(jLabel3)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel3)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(nomePesquisaTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(nomePesquisaBT))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                        .addGap(31, 31, 31))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(excluiBT)
+                        .addGap(33, 33, 33)
+                        .addComponent(novoBT)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 237, Short.MAX_VALUE)
+                        .addComponent(voltarBT)
+                        .addGap(34, 34, 34))))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void nomePesquisaTFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nomePesquisaTFActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_nomePesquisaTFActionPerformed
+
+    private void nomePesquisaBTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nomePesquisaBTActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_nomePesquisaBTActionPerformed
+
+    private void voltarBTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_voltarBTActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_voltarBTActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable estoqueTabela;
+    private javax.swing.JButton excluiBT;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton nomePesquisaBT;
+    private javax.swing.JTextField nomePesquisaTF;
+    private javax.swing.JButton novoBT;
+    private javax.swing.JButton voltarBT;
     // End of variables declaration//GEN-END:variables
 }
